dfd867b4512deaa8a8840ab821f7b904b32709fc
roksolana-shendiukh <roksolana.shendiukh@gmail.com>
2026-06-30 19:19:31 +0300

    refactor(models): update FlightCrewModel fromJson keys to snake_case

--- lib/models/flight_crew_model.dart
+++ lib/models/flight_crew_model.dart
@@ -1,13 +1,13 @@
 class FlightCrewModel {
-  final int     flightCrewId;
+  final int flightCrewId;
   final String? firstName;
   final String? lastName;
   final String? position;
-  final int?    positionId;
+  final int? positionId;
   final String? licenseType;
-  final int?    licenseTypeId;
-  final int?    experienceYears;
-  final bool    locationKnown;
+  final int? licenseTypeId;
+  final int? experienceYears;
+  final bool locationKnown;
 
   const FlightCrewModel({
     required this.flightCrewId,
@@ -24,18 +24,17 @@ class FlightCrewModel {
   String get fullName => '${firstName ?? ''} ${lastName ?? ''}'.trim();
 
   factory FlightCrewModel.fromJson(Map<String, dynamic> json) => FlightCrewModel(
-        flightCrewId:    (json['flightCrewId'] as num).toInt(),
-        firstName:       json['firstName']       as String?,
-        lastName:        json['lastName']        as String?,
-        position:        json['position']        as String?,
-        positionId:      (json['positionId']     as num?)?.toInt(),
-        licenseType:     json['licenseType']     as String?,
-        licenseTypeId:   (json['licenseTypeId']  as num?)?.toInt(),
-        experienceYears: (json['experienceYears'] as num?)?.toInt(),
-        locationKnown:   json['locationKnown']   as bool? ?? true,
+        flightCrewId:    (json['flight_crew_id'] as num).toInt(),
+        firstName:       json['first_name']        as String?,
+        lastName:        json['last_name']         as String?,
+        position:        json['position']          as String?,
+        positionId:      (json['position_id']      as num?)?.toInt(),
+        licenseType:     json['license_type']      as String?,
+        licenseTypeId:   (json['license_type_id']  as num?)?.toInt(),
+        experienceYears: (json['experience_years'] as num?)?.toInt(),
+        locationKnown:   json['location_known']    as bool? ?? true,
       );
 }
-
 
 class CrewValidationModel {
   final bool valid;
