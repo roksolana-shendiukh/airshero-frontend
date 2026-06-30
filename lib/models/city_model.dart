7969ba5a0e32b2e23d9bcccce76c4bda7daf112c
roksolana-shendiukh <roksolana.shendiukh@gmail.com>
2026-06-30 19:03:41 +0300

    refactor(models): add CityModel aligned to DB schema

--- lib/models/city_model.dart
+++ lib/models/city_model.dart
@@ -1,21 +1,20 @@
 class CityModel {
   final int cityId;
-  final String cityName;
-  final String countryName;
+  final int? countryId;
+  final String? countryName; 
+  final String? cityName;
 
   const CityModel({
     required this.cityId,
-    required this.cityName,
-    required this.countryName,
+    this.countryId,
+    this.countryName,
+    this.cityName,
   });
 
-  factory CityModel.fromJson(Map<String, dynamic> json) {
-    return CityModel(
-      cityId: json['cityId'] as int,
-      cityName: json['cityName'] as String,
-      countryName: json['countryName'] as String,
-    );
-  }
-
-  String get displayName => '$cityName, $countryName';
+  factory CityModel.fromJson(Map<String, dynamic> json) => CityModel(
+        cityId:      (json['city_id'] as num).toInt(),
+        countryId:   (json['country_id'] as num?)?.toInt(),
+        countryName: json['country_name'] as String?,
+        cityName:    json['city_name'] as String?,
+      );
 }
