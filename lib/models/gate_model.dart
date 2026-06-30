8d4f0223b0922354be150967931474ebb1f542bb
roksolana-shendiukh <roksolana.shendiukh@gmail.com>
2026-06-30 19:07:01 +0300

    refactor(models): add GateModel aligned to DB schema

--- lib/models/gate_model.dart
+++ lib/models/gate_model.dart
@@ -1,32 +1,17 @@
 class GateModel {
   final int gateId;
-  final String gateCode;
   final int terminalId;
-  final String? terminalCode;
-  final double? terminalSize;
-  final String? terminalType;
-  final bool isAvailable;
+  final String gateCode;
 
   const GateModel({
     required this.gateId,
-    required this.gateCode,
     required this.terminalId,
-    this.terminalCode,
-    this.terminalSize,
-    this.terminalType,
-    this.isAvailable = true,
+    required this.gateCode,
   });
 
   factory GateModel.fromJson(Map<String, dynamic> json) => GateModel(
-        gateId:       (json['gateId'] as num).toInt(),
-        gateCode:     json['gateCode'] as String,
-        terminalId:   (json['terminalId'] as num).toInt(),
-        terminalCode: json['terminalCode'] as String?,
-        terminalSize: (json['terminalSize'] as num?)?.toDouble(),
-        terminalType: json['terminalType'] as String?,
-        isAvailable:  json['isAvailable'] as bool? ?? true,
+        gateId:     (json['gate_id'] as num).toInt(),
+        terminalId: (json['terminal_id'] as num).toInt(),
+        gateCode:   json['gate_code'] as String,
       );
-
-  String get label =>
-      terminalCode != null ? 'T$terminalCode · Gate $gateCode' : 'Gate $gateCode';
 }
