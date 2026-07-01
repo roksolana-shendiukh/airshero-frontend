2ddd391ee6c2df8ef1b1d1b909dd6b862f246f6d
roksolana-shendiukh <roksolana.shendiukh@gmail.com>
2026-07-01 23:36:56 +0300

    refactor(models): update FlightOperationStatusModel fromJson keys to snake_case

--- lib/models/flight_operation_status_model.dart
+++ lib/models/flight_operation_status_model.dart
@@ -9,7 +9,7 @@ class FlightOperationStatusModel {
 
   factory FlightOperationStatusModel.fromJson(Map<String, dynamic> json) =>
       FlightOperationStatusModel(
-        statusId:   (json['flightOperationStatusId'] as num).toInt(),
-        statusName: json['flightOperationStatusName'] as String,
+        statusId:   (json['flight_operation_status_id'] as num).toInt(),
+        statusName: json['flight_operation_status_name'] as String,
       );
 }
